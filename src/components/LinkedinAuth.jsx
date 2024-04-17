d592781e899e9de1e12db55366a194d10a349b48
Alex Marin Mendez <alexmarinmendez@gmail.com>
2024-04-16 23:36:46 -0500

    fix: change http secure protocole in axios request at LinkedinAuth component

--- src/components/LinkedinAuth.jsx
+++ src/components/LinkedinAuth.jsx
@@ -7,7 +7,7 @@ const LinkedinAuth = () => {
     useEffect(() => {
         async function fetchData() {
             try {
-                const response = await axios("http://s14-01-m-node-react.vercel.app/api/v1/signin-with-linkedin/authorize");
+                const response = await axios("https://s14-01-m-node-react.vercel.app/api/v1/signin-with-linkedin/authorize");
                 console.log(response);
             } catch (err) {
                 console.log(err)
